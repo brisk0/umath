c5aa02b2c182d40dd836ad39f83d47b238e0f3e6
brisk <bskblake@gmail.com>
2018-08-04 23:53:00 +0930

    Remove prompt from output of non-interactive uses
    Program was printing a prompt and it's input before printing output for
    any input mode. Now changed to only use readline (and thus prompt) in
    interactive mode. This allows cleaner command-line use.
    E.g. in vim the visual :! command can now be used with umath to
    instantly convert a line or lines into their umath equivalents

--- main.c
+++ main.c
@@ -2,17 +2,39 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <errno.h>
 #include "umath.h"
 
-int main(int argc, char *argv[]) {
-	char *input;
+#define _POSIX_C_SOURCE 200809L
 
-	if(argc < 2) {
+int main(int argc, char *argv[]) {
+	char *input = NULL;
+
+	// Input modes
+	if(argc >= 2) {
+		// Cmdline argument
+		input = argv[1];
+	} else if(isatty(fileno(stdin))) {
+		// Interactive terminal
 		input = readline("> ");
 	} else {
-		input = argv[1];
+		// Pipe
+		size_t n = 0;
+		ssize_t result = getline(&input, &n, stdin);
+		if(result == -1) {
+			if(errno == EINVAL) {
+				fprintf(stderr, "Bad argument to getline()\n");
+			} else if (errno == ENOMEM) {
+				fprintf(stderr, "Line allocation failed\n");
+			}
+		}
+		// Strip newline
+		if(input[result - 1] == '\n') {
+			input[result - 1] = '\0';
+		}
 	}
 
 	if(!input) {
